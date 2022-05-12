038dc93eb504051d1d140cb5c88cf322f4c45b3b
nicolasleger1328 <nicolas.leger1328@gmail.com>
2022-05-12 14:39:13 +0100

    120522

--- Ajax/Exemples/exemple6 - test/index.js
+++ Ajax/Exemples/exemple6 - test/index.js
@@ -4,7 +4,7 @@ let container = document.querySelector(".container");
 
 function onclick(){
     console.log("ca marche");
-    fetch('https://api.nasa.gov/EPIC/api/natural/images?api_key=T9hVNyzvP2Zc0GNhKmPO8SDy0T6N7TeePjBEbMlJ')
+    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=3466&api_key=T9hVNyzvP2Zc0GNhKmPO8SDy0T6N7TeePjBEbMlJ')
         .then(response => {console.log(response);
             return response.json(); 
         })
@@ -12,7 +12,7 @@ function onclick(){
             console.log(photos);
             console.log(photos.photos[0]);
             console.log(photos.constructor.name);
-            for(let i=150; i<300; i++){
+            for(let i=200; i<442; i++){
             // for (const item of photos){
             //     console.log(item);
             // }
@@ -21,7 +21,7 @@ function onclick(){
                     
                     container.innerHTML += 
                    `
-                   <img src=${photos.photos[i].img_src} alt="">
+                   <img src=${photos.photos[i].img_src}  alt="">
                         
                         
                     `
---
+++ Ajax/Exemples/exemple7 - Geo et meteo/geo3.js
@@ -0,0 +1,85 @@
+let container = document.querySelector(".container");
+function gps(position){
+    let plat = document.querySelector(".latit");
+    let plon = document.querySelector(".long");
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    plat.textContent= "la latitude est "+lat;
+    plon.textContent= "la longitude est "+long;
+    
+}
+
+function showError(error){
+    console.log(error);
+}
+
+
+
+function getUserLocation(){
+    return new Promise((resolve, reject)=>{
+        if (!navigator.geolocation){
+            return reject("API geolocation non dispo");
+        }
+        navigator.geolocation.getCurrentPosition(position=>{
+            resolve(position);
+        }, error=>{
+            reject(error.message);
+        });
+    });
+}
+
+// encore plus simplifié
+
+function getUserLocation1(){
+    return new Promise((resolve, reject)=>{
+        if (!navigator.geolocation){
+            return reject({message:"API geolocation non dispo"});
+        }
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+}
+//Essai
+
+async function  getWeather(position){
+
+    
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    const response = await fetch(`http://api.weatherstack.com/current?access_key=ef1c86fbc44c37546799531b7d429e19&query=${lat},${long}`);
+    console.log(response);
+    const test1 = await response.json();
+    console.log(test1);
+    return test1;
+        // .then(response => {console.log(response);
+        //     return response.json();
+        // })
+        // .then(meteo => { console.log(meteo);
+        //     resolve(meteo)})
+    ;
+        
+
+        
+}
+function weatherAffiche(meteo){
+    container.innerHTML += 
+
+                               `    <h2>${meteo.location.name}, ${meteo.location.country}, ${meteo.location.region}</h2>
+                                    <p>Situation à ${meteo.current.observation_time}<p>
+                                    <p>Temperature : ${meteo.current.temperature}°C</p>
+                                    <p>Humidité : ${meteo.current.humidity}%</p>
+                                    <p>Indice UV: ${meteo.current.uv_index}</p>
+                                    <p>Pression Atmo: ${meteo.current.pressure} hP</p>
+                                    <p><img src="${meteo.current.weather_icons}" alt="">
+                                    
+                                    
+                                    
+                                `
+}
+getUserLocation1()
+    .then(position => {
+        gps(position);
+        return getWeather(position)})
+    .then(meteo => {
+        weatherAffiche(meteo);
+                })
+    .catch(showError)
